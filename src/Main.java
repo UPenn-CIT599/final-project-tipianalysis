17101666fc5b56d065796eee61b35a8395a864fe
cfernandes <clafer@seas.upenn.edu>
2019-11-07 19:26:35 +0100

    Updated gigignore added comments to main

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        //Mock-up: How to transition to results page
+        HBox hbox = new HBox();
+        Scene scene = new Scene(hbox);
+        //button to open results
+        Button click = new Button("Open Results");
+
+        //create user and resutlsviewer object
+        User user = new User("Tester", 30, "male");
+        ResultsViewer resultsViewer = new ResultsViewer(user);
+        click.setOnAction(e -> resultsViewer.start(primaryStage));
+        hbox.getChildren().addAll(click);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+}
