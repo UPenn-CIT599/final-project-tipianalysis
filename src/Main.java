382cabdb38390ff84f5281ef6e9a65a81bdcabd8
cfernandes <clafer@seas.upenn.edu>
2019-11-08 21:15:14 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        //Mock-up: How to transition to results page
+        //Mock-up: How to transition to results page hello
         HBox hbox = new HBox();
         Scene scene = new Scene(hbox);
         //button to open results
