5308bdd9728de1cfeb641fdd3ffa2d6103e591bb
cfernandes <clafer@seas.upenn.edu>
2019-11-09 14:42:01 +0100

    Set Getter Setter to User for TipiScore

--- src/User.java
+++ src/User.java
@@ -1,10 +1,13 @@
+import java.util.HashMap;
+
 /** Profile of User who is filling out TIPI Analysis */
 public class User {
 
-    // instance variables
+  // instance variables
   int age;
   private String name;
   private String sex;
+  private HashMap<String, Double> TipiScore;
 
   // Constructor
   public User(String name, int age, String sex) {
@@ -13,18 +16,30 @@ public class User {
     this.sex = sex;
   }
 
-  //Getter for instance variables
+  // Getter for instance variables
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getSex() {
-        return sex;
-    }
+  public String getSex() {
+    return sex;
+  }
 
+  public HashMap<String, Double> getTipiScore() {
+    return TipiScore;
+  }
+
+  /**
+   * Setter for tipi Score
+   *
+   * @param tipiScore
+   */
+  public void setTipiScore(HashMap<String, Double> tipiScore) {
+    this.TipiScore = tipiScore;
+  }
 }
