8fd4749a2298a9ec3887b20e7f19f892162f3ec8
amcman <amcman@seas.upenn.edu>
2019-11-12 00:05:07 -0500

    added all files (Junit library and slightly updated LogicTestCheck.Java file.

--- src/LogicCheckTest.java
+++ src/LogicCheckTest.java
@@ -10,8 +10,9 @@ class LogicCheckTest {
 
 	LogicCheck tester = new LogicCheck();
 	
+	
 	@Test
-	void testRecodeReverseScoredItems() {
+	public void testRecodeReverseScoredItems() {
 		
 		HashMap<String, Integer> inputResponses;
 		HashMap<String, Integer> outputsToMatch;
@@ -21,29 +22,29 @@ class LogicCheckTest {
 		inputResponses = new HashMap<String, Integer>();
 		// Assign input values to each of the 10 items in TIPI
 		inputResponses.put("Extraverted", 5);
-		inputResponses.put("Critical", 7);
+		inputResponses.put("Critical", 4);
 		inputResponses.put("Dependable", 6);
 		inputResponses.put("Anxious", 2);
 		inputResponses.put("Open", 5);
-		inputResponses.put("Reserved", 1);
+		inputResponses.put("Reserved", 3);
 		inputResponses.put("Sympothetic", 7);
-		inputResponses.put("Disorganized", 3);
+		inputResponses.put("Disorganized", 5);
 		inputResponses.put("Calm", 2);
-		inputResponses.put("Conventional", 3);
+		inputResponses.put("Conventional", 7);
 		
 		outputsToMatch = new HashMap<String, Integer>();
 		/* These values are what the input values should evaluate to after
 		going through the "RecodeReverseScoredItems" method. */
 		outputsToMatch.put("Extraverted", 5);
-		outputsToMatch.put("Critical", 1);
+		outputsToMatch.put("Critical", 4);
 		outputsToMatch.put("Dependable", 6);
 		outputsToMatch.put("Anxious", 6);
 		outputsToMatch.put("Open", 5);
-		outputsToMatch.put("Reserved", 7);
+		outputsToMatch.put("Reserved", 5);
 		outputsToMatch.put("Sympothetic", 7);
-		outputsToMatch.put("Disorganized", 5);
+		outputsToMatch.put("Disorganized", 3);
 		outputsToMatch.put("Calm", 2);
-		outputsToMatch.put("Conventional", 5);
+		outputsToMatch.put("Conventional", 1);
 		
 		System.out.println(inputResponses);
 		System.out.println(outputsToMatch);
