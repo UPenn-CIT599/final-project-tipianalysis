6c781ddbfc2ccadcb92538c64b055665c346db81
cfernandes <clafer@seas.upenn.edu>
2019-12-05 19:27:43 +0100

    Commented User Test Cases

--- src/UserTest.java
+++ src/UserTest.java
@@ -7,18 +7,27 @@ class UserTest {
     private User user = new User("Bob", 100, "male");
 
     @Test
+    /**
+     * Tests Functionality to Set Name of User
+     */
     void testSetName() {
         user.setName("Alice");
         assertEquals("Alice",user.getName());
     }
 
     @Test
+    /**
+     * Tests Functionality to Set Age of User
+     */
     void testSetAge() {
         user.setAge(50);
         assertEquals(50,user.getAge());
     }
 
     @Test
+    /**
+     * Tests Functionality to Set Sex of User
+     */
     void testSetSex() {
         user.setSex("female");
         assertEquals("female",user.getSex());
@@ -26,11 +35,17 @@ class UserTest {
 
 
     @Test
+    /**
+     * Tests Functionality to Get User Scores and Metrics
+     */
     void testGetUserScoresAndMetrics() {
         assertEquals(null,user.getUserScoresAndMetrics());
     }
 
     @Test
+    /**
+     * Tests Functionality to Set User Scores and Metrics
+     */
     void setUserScoresAndMetrics() {
         Trait[] traits = new Trait[5];
         for(int i = 0; i<5;i++){
